05e80b8c1aab14eee57ca227bce5deb4be81dbe3
henrique-pignatari <henrique.pignatarimoraes@gmail.com>
2025-07-20 05:56:22 -0300

    feat: adds tasks service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskData, Task } from './task/task.model';
-import { DUMMY_TASKS } from '../dummy-tasks';
+import { Component, inject, Input } from '@angular/core';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskComponent } from './task/task.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -14,33 +13,19 @@ import { NewTaskComponent } from './new-task/new-task.component';
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  private readonly tasksService = inject(TasksService);
+
   isAddingTask: boolean = false;
-  tasks: Task[] = DUMMY_TASKS;
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
-  }
-
-  onCompleteTask(id: string) {
-    this.tasks.filter((task) => task.id !== id);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-    });
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
